55b4bf57a68e9f177f5e76b63633874243954fc4
Leandro Ventura <leandroventur19@gmail.com>
2025-05-11 08:07:47 -0300

    setup project

---
+++ furniture-ecommerce-app/app/(tabs)/_layout.tsx
@@ -0,0 +1,62 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { Tabs } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
+
+export default function DefaultLayout() {
+    const tabTintColor = useThemeColor({ light: Colors.light.tabIconSelected, dark: Colors.dark.tabIconSelected }, 'tint');
+    const tabInactiveColor = useThemeColor({ light: Colors.light.tabIconDefault, dark: Colors.dark.tabIconDefault }, 'tint');
+    const tabBackgroundColor = useThemeColor({ light: Colors.light.background, dark: Colors.dark.background }, 'background');
+
+    const renderIcon = (name: string, focused: boolean) => (
+        <View>
+            <Ionicons
+                name={name as any}
+                size={24}
+                color={focused ? Colors.light.tabIconSelected : Colors.light.tabIconDefault}
+            />
+        </View>
+    );
+
+    return (
+        <Tabs initialRouteName="index" screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarActiveTintColor: tabTintColor,
+            tabBarInactiveTintColor: tabInactiveColor,
+            tabBarStyle: {
+                backgroundColor: tabBackgroundColor,
+                borderTopWidth: 0,
+            },
+            tabBarShowLabel: false,
+        })}>
+
+            <Tabs.Screen name="index" options={{
+                tabBarIcon: ({ focused }) =>
+                    renderIcon(focused ? "home" : "home-outline", focused),
+            }} />
+
+            <Tabs.Screen name="messages" options={{
+                tabBarIcon: ({ focused }) =>
+                    renderIcon(focused ? "mail" : "mail-outline", focused),
+            }} />
+
+            <Tabs.Screen name="shop" options={{
+                tabBarIcon: ({ focused }) =>
+                    renderIcon(focused ? "bag" : "bag-outline", focused),
+            }} />
+
+            <Tabs.Screen name="favorite" options={{
+                tabBarIcon: ({ focused }) =>
+                    renderIcon(focused ? "heart" : "heart-outline", focused),
+            }} />
+
+            <Tabs.Screen name="profile" options={{
+                tabBarIcon: ({ focused }) =>
+                    renderIcon(focused ? "person" : "person-outline", focused),
+            }} />
+
+
+        </Tabs>
+    );
+}
---
+++ furniture-ecommerce-app/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <>
+    </>
+  );
+}
+
+
+const styles = StyleSheet.create({
+
+});
